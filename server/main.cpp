5f0cd98cb0c32f96b0c143afbad456f1c2ef9c30
xiaohan_orange <289775283@qq.com>
2018-09-01 13:12:36 +0800

    add db init in server init

--- server/main.cpp
+++ server/main.cpp
@@ -1,14 +1,30 @@
-// #include <dlfcn.h>
+#include <stdlib.h>
+#include "common/flags.h"
+#include "common/log.h"
+#include "database/sqlite_wrapper.h"
 #include "server/server.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using std::string;
+using namespace easylogin::common;
+using namespace easylogin::database;
 using namespace easylogin::server;
 
-const static string ADDR = "0.0.0.0:50051";
-const static string LISTEN = "Server listen on ";
+static const char* ADDR = "0.0.0.0:50051";
+
+static void InitDb();
+static void InitServer();
+static void RunServer();
+
+int main(int argc, char* argv[])
+{
+    InitServer();
+    RunServer();
+
+    return 0;
+}
 
 static void RunServer()
 {
@@ -19,12 +35,35 @@ static void RunServer()
             grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << LISTEN << server_address << std::endl;
+    LOG_INFO("Server run success!");
     server->Wait();
 }
 
-int main(int argc, char* argv[])
+static void InitServer()
 {
-    RunServer();
-    return 0;
+    InitDb();
+}
+
+static void InitDb()
+{
+    SqliteWrapper mySqlite;
+    DbStatus status = mySqlite.Open(TEST_DB_FILE);
+    if (!status.IsOk())
+    {
+        LOG_ERROR(status.ToString().c_str());
+        exit(1);
+    }
+    string sql = "CREATE TABLE IF NOT EXISTS USERINFO("      \
+                 "USERNAME CHAR(128) PRIMARY KEY NOT NULL,"  \
+                 "PASSWORD CHAR(512) NOT NULL,"              \
+                 "TOKEN CHAR(258) NOT NULL)";
+
+    status = mySqlite.Execute(sql);
+    if (!status.IsOk())
+    {
+        LOG_ERROR("Create table error");
+        LOG_ERROR(status.ToString().c_str());
+        exit(1);
+    }
+    LOG_INFO("Db init success!");
 }
